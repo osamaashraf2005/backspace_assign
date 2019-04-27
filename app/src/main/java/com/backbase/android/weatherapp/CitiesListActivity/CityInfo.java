5e6f31e5921011b6bc4e209997a6a2f052175682
Sam <osama.ashraf2005@gmail.com>
2019-04-27 12:38:51 +0300

    Refactoring the cityInfo bean

--- app/src/main/java/com/backbase/android/weatherapp/CitiesListActivity/CityInfo.java
+++ app/src/main/java/com/backbase/android/weatherapp/CitiesListActivity/CityInfo.java
@@ -51,4 +51,30 @@ public class CityInfo
     {
         this.coordinate = coordinate;
     }
+
+    private class Coordinate
+    {
+        private long lat;
+        private long lon;
+
+        public long getLat()
+        {
+            return lat;
+        }
+
+        public void setLat(long lat)
+        {
+            this.lat = lat;
+        }
+
+        public long getLon()
+        {
+            return lon;
+        }
+
+        public void setLon(long lon)
+        {
+            this.lon = lon;
+        }
+    }
 }
